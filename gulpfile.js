ff12762d83813e247a90c0b863a4b005be9ce9ce
Zachary Napfel <znapfel@gmail.com>
2019-07-23 12:31:32 -0400

    support for font pickers

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const replace = require('gulp-replace');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
 
 // Compile scss into scss.liquid file
 function compileLiquid() {
     // Get SCSS files
-    return gulp.src('./src/scss/**/*.scss')
+    return gulp.src('./src/scss/theme.scss')
     // Pass it through sass compiler
-    .pipe(sass())
+    .pipe(sass({ 
+        outputStyle: 'compressed '
+    })
+    .on('error', sass.logError))
+    // Add vendor prefixes
+    .pipe(autoprefixer())
+    .pipe(rename('theme.scss.liquid'))
+ 
     // Save the compiled scss.liquid
-    .pipe(gulp.dest('./assets/theme.scss.liquid'));
+    .pipe(gulp.dest('./assets/'));
 
 }
 
